141d6764f244b8a25f800e9d86275df1d53b8619
Alvaro Mateo <alvaromateo9@gmail.com>
2025-07-11 18:01:39 +0200

    Modify express-app to use Vite SSR

--- express-app/src/App.tsx
+++ express-app/src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from '/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +8,10 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://reactjs.org" target="_blank">
+          <img src="/react.svg" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
